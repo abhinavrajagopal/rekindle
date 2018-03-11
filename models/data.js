248a3442b894e1f4a21c3feea6ef4f21189a5aaa
Harshal Varday <harshalvarday1@gmail.com>
2018-03-11 01:21:34 -0500

    minor fix

--- models/data.js
+++ models/data.js
@@ -2,9 +2,9 @@ var mongoose = require('mongoose');
 
 var temp = mongoose.Schema({
     room : { type : String, required : true, default : "100" },
-    val1 : { type : String, required : true, default : "0" },
-    val2 : { type : String, required : true, default : "0" },
-    val3 : { type : String, required : true, default : "0" },
+    value1 : { type : String, required : true, default : "0" },
+    value2 : { type : String, required : true, default : "0" },
+    value3 : { type : String, required : true, default : "0" },
 },{
     timestamps : true
 })
