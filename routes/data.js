af3984d4ad0c318925cd0798f96d4ffa45715c30
Harshal Varday <harshalvarday1@gmail.com>
2018-03-11 04:01:01 -0400

    Query parameter change

--- routes/data.js
+++ routes/data.js
@@ -4,7 +4,11 @@ var router = require('express').Router(),
 router.route("/")
 .get((req, res, next)=>{
     
-    new dataModel(req.query).save()
+    new dataModel({
+        value1 : req.query.value,
+        value2 : req.query.value1,
+        value3 : req.query.value2,
+    }).save()
     .then(()=>res.send("OK"))
     .catch(next);
 
